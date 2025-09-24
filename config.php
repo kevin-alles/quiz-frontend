33476c592ca89f4219852a9e8aff11934328f1b7
Kevin Alles <git-message@kevin-alles.de>
2025-09-24 14:49:50 +0200

    Add GitHub Actions workflow to trigger webhook on push events

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// config.php
+// Configuration file for server settings
+
+return [
+    'server_url' => 'quiz.local.it-mit-alles.de',
+    'server_port' => 8080,
+];
+
--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php
-phpinfo();
+// Include the config file
+$config = require __DIR__ . '/config.php';
+
+$serverUrl = $config['server_url'];
+$serverPort = $config['server_port'];
+
+$options = [
+    'http' => [
+        'method' => 'POST',
+    ],
+];
+
+// Create stream context
+$context = stream_context_create($options);
+$endpoint = "http://$serverUrl:$serverPort/reachable";
+
+// Perform POST request
+$response = file_get_contents($endpoint, false, $context);
+
+// Display the response
+echo $response;
+
 ?>
+
